4950a96b8233a0753a76e6213c3f30a8cc3af610
goldfest <vano00189@mail.ru>
2026-08-17 20:18:49 +0400

    fix: исправлен JWT-секрет для тестов

--- src/test/java/ru/putevodika/PutevodikaBackendApplicationTests.java
+++ src/test/java/ru/putevodika/PutevodikaBackendApplicationTests.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"app.security.jwt.secret=MDEyMzQ1Njc4OTAxMjM0NTY3ODkwMTIzNDU2Nzg5MDE="
+})
 @Import(TestcontainersConfiguration.class)
 class PutevodikaBackendApplicationTests {
 
